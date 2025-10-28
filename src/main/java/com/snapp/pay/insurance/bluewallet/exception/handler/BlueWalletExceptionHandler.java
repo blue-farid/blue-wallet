0a834fd38916ed3f60db4d92e73189d0e0f4f760
faird.masjedi <farid.masjedi>
2025-10-28 17:27:28 +0330

    feat: add base exception handler

---
+++ src/main/java/com/snapp/pay/insurance/bluewallet/exception/handler/BlueWalletExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.snapp.pay.insurance.bluewallet.exception.handler;
+
+import com.snapp.pay.insurance.bluewallet.api.v1.model.ApiResponse;
+import com.snapp.pay.insurance.bluewallet.api.v1.model.ErrorResponse;
+import com.snapp.pay.insurance.bluewallet.constant.ApiStatus;
+import com.snapp.pay.insurance.bluewallet.exception.BaseException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+//TODO could add more exception handlers method. for now just this (RUNNING OUT OF TIME!)
+
+@Slf4j
+@ControllerAdvice
+public class BlueWalletExceptionHandler {
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleBaseException(BaseException ex) {
+        log.error("Business exception", ex);
+        return buildResponse(ex.getStatus(), ex.getMessage());
+    }
+
+    private ResponseEntity<ApiResponse<ErrorResponse>> buildResponse(HttpStatusCode status, String message) {
+        return new ResponseEntity<>(new ApiResponse<>(ApiStatus.FAILURE.name(), new ErrorResponse(message)), status);
+    }
+}
