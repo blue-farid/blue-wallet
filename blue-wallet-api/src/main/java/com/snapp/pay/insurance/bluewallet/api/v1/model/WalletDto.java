145e7df48ee538895af79e903768f2584ec57b43
faird.masjedi <farid.masjedi>
2025-10-31 06:01:15 +0330

    feat: add some accessors for the tests

--- blue-wallet-api/src/main/java/com/snapp/pay/insurance/bluewallet/api/v1/model/WalletDto.java
+++ blue-wallet-api/src/main/java/com/snapp/pay/insurance/bluewallet/api/v1/model/WalletDto.java
@@ -2,10 +2,12 @@ package com.snapp.pay.insurance.bluewallet.api.v1.model;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 import java.math.BigDecimal;
 
 @Data
+@Accessors(chain = true)
 public class WalletDto {
     private Long id;
     @NotNull
--- blue-wallet-api/src/main/java/com/snapp/pay/insurance/bluewallet/api/v1/request/admin/CreateWalletRequest.java
+++ blue-wallet-api/src/main/java/com/snapp/pay/insurance/bluewallet/api/v1/request/admin/CreateWalletRequest.java
@@ -2,8 +2,10 @@ package com.snapp.pay.insurance.bluewallet.api.v1.request.admin;
 
 import com.snapp.pay.insurance.bluewallet.api.v1.model.WalletDto;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class CreateWalletRequest {
     private WalletDto wallet;
 }
